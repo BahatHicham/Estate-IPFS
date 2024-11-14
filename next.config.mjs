973bfb8a3ba1f22cf325f76e0b008d20fe3195ee
BahatHicham <159668004+BahatHicham@users.noreply.github.com>
2024-11-14 19:13:44 +0100

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
     images: {
         domains: ['rose-total-dinosaur-689.mypinata.cloud'],
-        unoptimized: true, // Needed for GitHub Pages compatibility with Next.js images
       },
       // sassOptions: {
       //   includePaths: [path.join(__dirname, 'styles')],
